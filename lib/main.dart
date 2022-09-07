12089fa270f49597b3a9e8656d58e8024332f2af
anujpuri76 <purianuj603@gmail.com>
2022-09-07 11:05:15 +0100

    initial setup

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+// import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: HomePage(),
+
+      );}
+}
+
+
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("VIP numberstore"),
+      ),
+      drawer: Drawer(
+        
+      ),
+    );
+    
+  }
+}
